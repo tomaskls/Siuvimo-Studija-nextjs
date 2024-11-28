941384ffc36de110f6f91e34c5177566c127760c
tomaskls <tomaskls@hotmail.com>
2024-11-28 11:31:20 +0200

    redirect old pages

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,27 @@ const nextConfig: NextConfig = {
         destination: '/drabuziu-taisymas',
         permanent: true,
       },
-      // čia galite pridėti daugiau nukreipimų
+      {
+        source: '/sewing',
+        destination: '/siuvykla',
+        permanent: true,
+      },
+      {
+        source: '/product',
+        destination: '/flisiniai-dzemperiai',
+        permanent: true,
+      },
+      {
+        source: '/contacts',
+        destination: '/kontaktai',
+        permanent: true,
+      },
+      {
+        source: '/prices',
+        destination: '/drabuziu-taisymo-kainos',
+        permanent: true,
+      },
+     
     ]
   },
 };
