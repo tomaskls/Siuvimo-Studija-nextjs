15790490d1034aaf2e8ed1b1ef54c3bbfb90e749
tomaskls <tomaskls@hotmail.com>
2024-12-20 14:19:57 +0200

    target blank

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,14 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
     return (
         <div className={style.footer}>Sukurta
-            <a className={style.tmh} href="www.tmh.lt">Tomorrow&apos;s Media House</a>
+            <a 
+                className={style.tmh} 
+                href="https://www.tmh.lt" 
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                Tomorrow&apos;s Media House
+            </a>
             © {currentYear} Neringos Siuvimo Studija. Visos teisės saugomos.
         </div>
     );
