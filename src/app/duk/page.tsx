40839bb26586afb9484c7534acd2d5742e6c5e33
tomaskls <tomaskls@hotmail.com>
2025-02-01 13:37:15 +0200

    duk update

--- src/app/duk/page.tsx
+++ src/app/duk/page.tsx
@@ -3,7 +3,30 @@ import React from 'react';
 import FAQList from '../../components/faq/FAQList';
 import { getFAQs } from '../../components/faq/faq';
 import { getFAQs2 } from '../../components/faq/faq';
-import styles from './duk.module.css'
+import styles from './duk.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Šilti flisiniai džemperiai",
+  description: "Sušilkite stilingai su unikaliais, pagal užsakymą siuvamais flisiniais džemperiais Šiauliuose! Rinkitės ir pritaikykite dydį, spalvą ir detales!",
+  openGraph: {
+      title: 'Neringos Siuvimo Studija | DUK',
+      description: 'Turite klausimų apie rūbų taisymą ar siuvimą? Mūsų DUK puslapyje rasite atsakymus apie drabužių korekcijas, užsakymų terminus ir kainas. Greita, aišku ir patogu!', 
+      url: 'https://www.neringos-siuvimo-studija.lt/duk',
+      siteName: 'Neringos Siuvimo Studija',
+      type: 'website',
+      images: [
+          {
+              url: 'https://www.neringos-siuvimo-studija.lt/Images/siltas_flisinis_dzemperis_900.webp', 
+              width: 900,
+              height: 1352,
+          },
+      ],
+  },
+  alternates: {
+      canonical: 'https://www.neringos-siuvimo-studija.lt/duk',
+  }
+};
 
 export default async function FAQPage() {
   const faqs = await getFAQs();
