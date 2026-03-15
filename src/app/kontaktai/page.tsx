66923e94fcfb5c1138b0bf32c7ae39dade2e5b99
tomaskls <tomaskls@hotmail.com>
2026-03-15 09:42:08 +0200

    laikas

--- src/app/kontaktai/page.tsx
+++ src/app/kontaktai/page.tsx
@@ -39,6 +39,7 @@ export default function Contacts() {
                     <h1>Kontaktai</h1>
                     <a href="tel:+37060055316">Skambinkite tel: +370 600 55316</a>
                     <p>Mus rasite adresu: <br />Vytauto g. 80 <br />Šiauliai</p>
+                    <p style={{ color: 'red' }}>Atsiprašome, kovo 16 dieną nedirbsime.</p>
                     <p>Planuojate apsilankyti? Mums būtų malonu, jei prieš tai paskambintumėte.<br />Taip galėsime užtikrinti, kad Jums nereikės laukti.</p>
                     </div>
             </div>
