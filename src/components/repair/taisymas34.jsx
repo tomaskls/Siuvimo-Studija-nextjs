dae69842bc2a6ff8cb08bf95426ecbdabd8d6a76
tomaskls <tomaskls@hotmail.com>
2026-06-30 13:50:22 +0300

    svarkas

--- src/components/repair/taisymas34.jsx
+++ src/components/repair/taisymas34.jsx
@@ -6,10 +6,10 @@ import Image from 'next/image';
 export default function Repair34() {
     return (
         <div className={style.container}>
-            <h3 className={style.h3}>✂️ Klasikinio kostiumo taisymas, rankovių trumpinimas, siaurinimas </h3>
+            <h3 className={style.h3}>✂️ Klasikinio kostiumo taisymas, švarko apačios trumpinimas </h3>
             <Image className={style.img}
                 src="/imgGallery/2026/IMG20260325105354.webp"
-                alt="Klasikinio kostiumo taisymas, rankovių trumpinimas, siaurinimas"
+                alt=" švarko apačios trumpinimas"
                 width={675}
                 height={1200}
                 priority={false}
